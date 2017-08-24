c2e468d1b2c0a1cb65b10d755b7ad5bb1401a61c
Lessan Vaezi <lessan@gmail.com>
2017-08-24 21:15:29 +1200

    Rename bash_profile to bash_aliases

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ DOTFILES=$(pwd)
 
 cd
 
-relink .bash_profile     $DOTFILES/bash_profile
+relink .bash_aliases     $DOTFILES/bash_aliases
 relink .gemrc            $DOTFILES/gemrc
 relink .gitconfig        $DOTFILES/gitconfig
 relink .vimrc            $DOTFILES/vimrc
